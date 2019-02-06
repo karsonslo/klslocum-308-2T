8f89e99e2809424dc6cac5697da55b056fe45724
karsonslo <karson.slocum2@gmail.com>
2019-02-06 14:25:56 -0800

    multiply added

---
+++ multiply/multiply.java
@@ -0,0 +1,18 @@
+package multiply;
+
+public class multiply {
+
+	public static int multiply(int a, int b) {
+		return a*b;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 3;
+		int c = 0;
+		c = multiply(a, b);
+		System.out.print(a + " x " + b + " = " + c);
+	}
+
+}
